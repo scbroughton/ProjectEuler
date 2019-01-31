93ab42630eee501604facb12e418bc3a37884646
scbroughton <scbroughton@users.noreply.github.com>
2019-01-31 01:08:43 -0600

    Added memoized algorithm and an object-based algorithm for comparison (otherwise, object lifecycle and packing/unpacking would give an unfair comparison).

--- src/ProjectEuler/Problem014.java
+++ src/ProjectEuler/Problem014.java
@@ -1,5 +1,10 @@
 package ProjectEuler;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * <h1>Problem 14</h1>
  *
@@ -26,25 +31,72 @@ package ProjectEuler;
  * @author Stephen Broughton
  * @since Aug 12, 2018
  */
-
 public class Problem014 {
+    private static final Hashtable<Long,Integer> CACHE = new Hashtable<>();
+    private static final int UPPER_BOUND = 1000000;
 
     public static void main(String[] args) {
-        int longestStart = 1;
+        long longestStart = 1;
         int longestCount = 0;
-        for (int i = 2; i < 1000000; i++) {
-            int count = 0;
-            for (long n = i; n > 1; count++)
-                n = collatz(n);
+        int count;
+        
+        System.out.println("-----------Memoized-----------");
+        long startTime = System.currentTimeMillis();
+        CACHE.put(1l, 0);
+        for (Long n = 2l; n < UPPER_BOUND; n++) {
+            CACHE.put(n, getHops(n));
+            count = CACHE.get(n);
             if (count > longestCount) {
-                longestStart = i;
+                longestStart = n;
                 longestCount = count;
             }
         }
-        System.out.println(longestStart);
+        System.out.println("Time taken:\t\t" + (System.currentTimeMillis() - startTime) + "ms");
+        System.out.println("Longest chain's index:\t" + longestStart);
+        System.out.println("Chain Length:\t\t" + longestCount);
+        System.out.println("Cached Entries:\t\t" + CACHE.size());
+        CACHE.clear();
+        
+        System.out.println("\n-----Direct (w/ Objects)------");
+        startTime = System.currentTimeMillis();
+        for (Long n = 2l; n < UPPER_BOUND; n++) {
+            count = 0;
+            for (Long i = n; i != 1; i = collatzObj(i)) count++;
+            if (count > longestCount) {
+                longestStart = n;
+                longestCount = count;
+            }
+        }
+        System.out.println("Time taken:\t\t" + (System.currentTimeMillis() - startTime) + "ms");
+        System.out.println("Longest chain's index:\t" + longestStart);
+        System.out.println("Chain Length:\t\t" + longestCount);
+
+        System.out.println("\n----Direct (w/ Primitives)----");
+        startTime = System.currentTimeMillis();
+        for (long n = 2l; n < UPPER_BOUND; n++) {
+            count = 0;
+            for (long i = n; i != 1; i = collatzPrim(i)) count++;
+            if (count > longestCount) {
+                longestStart = n;
+                longestCount = count;
+            }
+        }
+        System.out.println("Time taken:\t\t" + (System.currentTimeMillis() - startTime) + "ms");
+        System.out.println("Longest chain's index:\t" + longestStart);
+        System.out.println("Chain Length:\t\t" + longestCount);
+    }
+    
+    private static Integer getHops(Long n) {
+        if (!CACHE.containsKey(n)) CACHE.put(n, getHops(collatzObj(n)));
+        return CACHE.get(n) + 1;
     }
 
-    public static long collatz(long n) {
+    private static Long collatzObj(Long n) {
+        if (n % 2 == 0) return Long.valueOf(n / 2);
+        return Long.valueOf(3 * n + 1);
+    }
+    
+    private static long collatzPrim(long n) {
         if (n % 2 == 0) return n / 2;
         return 3 * n + 1;
     }
